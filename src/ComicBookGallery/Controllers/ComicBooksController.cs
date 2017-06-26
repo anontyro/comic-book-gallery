123655c3496c993e8abc65d3f88c55972de3bfb2
anontyro <alexwilkinson@gmail.com>
2017-06-26 22:47:16 +0800

    adding viewbag and updating the view controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,12 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            
+            ViewBag.SeriesTitle = "First Title";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>This is a description</p>";
+            ViewBag.Artists =new string[] { "first", "second" };
+
             return View();
         }
     }
